fc14c5523e1df9b82a32d95ddcc2a660c0af0e57
Mai Tran <maitran.ln99@gmail.com>
2022-02-06 11:32:48 +0200

    Switched to using json server

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 /* eslint-disable no-undef */
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
+// // Heroku dynamically sets a port
+// const PORT = process.env.PORT || 5000;
+
+// app.use(express.static("build"));
+
+// app.get("/health", (req, res) => {
+//   res.send("ok");
+// });
+
+// app.listen(PORT, () => {
+//   // eslint-disable-next-line no-console
+//   console.log("server started on port 5000");
+// });
+
+const jsonServer = requier("json-server");
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("build"));
+const fSystem = require("fs");
+const path = require("path");
+
+let rawData = fSystem.readFileSync(path.resolve(__dirname, "db.json"));
+let data = JSON.parse(rawData);
+
+const router = jsonServer.router(data);
+
+server.use(middlewares);
+server.use(router);
 
 app.get("/health", (req, res) => {
   res.send("ok");

de1f258e0db73e944d009ac926dfc77c78dac931
Mai Tran <maitran.ln99@gmail.com>
2022-02-06 11:42:11 +0200

    Fixed port number

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/anecdotes";
+const baseUrl = "http://localhost:5000/anecdotes";
 const getId = () => (100000 * Math.random()).toFixed(0);
 
 const getAll = async () => {
